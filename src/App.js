b1773d316747b4839ba2772aca73b82a40c2f048
Pav85 <pawel.werbowy@gmail.com>
2023-03-05 16:41:30 +0000

    added jumbotron component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import HomePage from "./components/HomePage/HomePage";
 import ContactPage from "./components/ContactPage/ContactPage";
 import Header from "./components/Header/Header";
 import ProjectsPage from "./components/ProjectsPage/ProjectsPage";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
 
 function App() {
   return (
     <div>
       <Header />
+      <Jumbotron />
       <HomePage />
       <ContactPage />
       <ProjectsPage />
