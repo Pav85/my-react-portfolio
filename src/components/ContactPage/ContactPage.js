884916d7a9733d0ade7b0166f478f644513e37a9
Pav85 <pawel.werbowy@gmail.com>
2023-03-07 08:36:28 +0000

    contact page update

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -1,10 +1,60 @@
 import React from "react";
 import ContactPageJumbotron from "../ContactPageJumbotron/ContactPageJumbotron";
+import "./ContactPage.css";
+import "https://kit.fontawesome.com/3c268dfe1e.js";
 
 function ContactPage() {
   return (
-    <div>
+    <div className="container">
       <ContactPageJumbotron />
+      <div className="row contactStyle">
+        <div className="col-sm">
+          <a href="mailto:pawel.werbowy@gmail.com" class="btn contactBtn">
+            <i class="fa fa-at"></i> Email me
+          </a>
+        </div>
+        <div className="col-sm">
+          <a
+            href="https://github.com/pav85"
+            target="_blank"
+            className="btn contactBtn"
+          >
+            <i className="fa fa-github"></i> GitHub
+          </a>
+        </div>
+        <div className="col-sm">
+          <a
+            href="https://docs.google.com/document/d/1hlYQQBNad4ctSdPuTKJDcf9bz0epDj0P/edit?usp=sharing&ouid=100228695758662225835&rtpof=true&sd=true"
+            target="_blank"
+            className="btn contactBtn"
+          >
+            <i className="fa fa-file"></i> Resume
+          </a>
+        </div>
+        <div className="col-sm">
+          <a
+            href="https://www.linkedin.com/in/pawel-werbowy-989ab2186/"
+            target="_blank"
+            className="btn contactBtn"
+          >
+            <i className="fa fa-linkedin"></i> LinkedIn
+          </a>
+        </div>
+        <div className="col-sm">
+          <a
+            href="https://www.facebook.com/pawel.werbowy/"
+            target="_blank"
+            class="btn contactBtn"
+          >
+            <i class="fa fa-facebook"></i> Facebook
+          </a>
+        </div>
+        <div className="col-sm">
+          <a href="tel: 00447510725530" target="_blank" class="btn contactBtn">
+            <i class="fa fa-phone"></i> Call me
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
+import "https://kit.fontawesome.com/3c268dfe1e.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
